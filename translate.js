c71e42ce7604318cee6a2d3f4be4dc10722ae5a7
delaneybaker <u694427660@byui.edu>
2023-11-11 13:17:08 -0600

    a

--- translate.js
+++ translate.js
@@ -16,3 +16,29 @@ const text = 'Hello, World!';
 const target = 'es'; // Target language: Spanish
 
 translateText(text, target);
+
+const { google } = require('googleapis');
+
+// Use the path to the downloaded JSON key file
+const keyFilePath = '/delaneybaker/downloads/coral-bucksaw-404818-08a184e3400f.json';
+
+// Load the key file
+const keyFile = require(keyFilePath);
+
+// Authenticate with the service account
+const jwtClient = new google.auth.JWT(
+    keyFile.client_email,
+    null,
+    keyFile.private_key,
+    ['https://www.googleapis.com/auth/devstorage.read_write'], // Specify the necessary scopes
+    null
+   );
+
+// Use the JWT client to authenticate with the APIs
+jwtClient.authorize((err, tokens) => {
+ if (err) {
+    console.log('Error:', err);
+    return;
+ }
+ console.log('Successfully authenticated with the Google Cloud APIs');
+});
